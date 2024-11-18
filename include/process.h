e60a18bc378dcfbbf278308b88421d7dbd97a552
doctuspullus <jarc1931@gmail.com>
2024-11-17 20:44:17 -0600

    Añadido getQuantum
    Añadido metodo getQuantum para obtener el quantum

--- include/process.h
+++ include/process.h
@@ -53,6 +53,9 @@ class Process {
   /// @brief Setter del estado del proceso.
   /// @param newState El nuevo estado al que pasa el proceso.
   void setState(ProcessState newState);
+  /// @brief Getter del quantum restante.
+  /// @return El quantum restante.
+  float getQuantum();
   /// @brief Setter para el quantum del proceso.
   /// @param quantum El nuevo quantum del proceso.
   void setQuantum(float newQuantum);
